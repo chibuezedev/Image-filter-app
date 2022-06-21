2b112940dab0d603beeedde1ea2a8d3c31e62630
Paul Chibueze <paulpadro03@gmail.com>
2022-06-21 11:16:22 +0100

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express from 'express';
+import express, { Application } from 'express';
+import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+
 
 (async () => {
 
   // Init the Express application
-  const app = express();
+  const app: Application = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
@@ -29,24 +32,28 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
   
-  app.get("/filteredimage",async(req,res)=>{
+  app.get("/filteredimage",async(req: Request, res: Response)=>{
     console.log("in");
-    let { image_url } = req.params.image_url;
+    let  image_url : string = req.query.image_url;
+
     //Validate url
-    const isValideUrl = image_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if(isValideUrl == null)
+  
+    const isValideUrl   = image_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+
+    if( isValideUrl == null)
+
       return res.status(400).send(`Inavlid url! Try again with valid url`);
     else{
     //send the resulting file in response
       const filteredImage = filterImageFromURL(image_url);
-      if(filteredImage===undefined||filteredImage===null)
+      if( filteredImage === undefined || filteredImage === null )
       return res.status(400).send(`Unable to filter image`);
     else
-      return res.status(200).sendFile(filteredImage+'');
+      return res.status(200).sendFile( filteredImage + '');
     }
   })
   
-  app.post("/delete-files", async (request, res) => {
+  app.post("/delete-files", async (req: Request, res: Response) => {
     deleteLocalFiles(getTempFiles());
     res.status(200).send("All image files have been removed");
   });
@@ -59,7 +66,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async (req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
